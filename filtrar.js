b40a3d46296cfe387761e38ac25761e775e8dd23
clebera1 <clebergressoni80@gamil.com>
2022-12-10 18:33:13 -0300

    fixing bugs

--- filtrar.js
+++ filtrar.js
@@ -1,6 +1,6 @@
 const filter = document.getElementById('filter')
 filter.addEventListener('keyup' , () => {
-    let expressao = filter.value.toLocaleLowerCase;
+    let expressao = filter.value;
 
     if(expressao.lenght < 2){
         return; 
@@ -14,8 +14,8 @@ filter.addEventListener('keyup' , () => {
             continue;
         }
 
-        let conteudoDaLinha = linhas[posicao].innerHTML.toLocaleLowerCase;
-        if (true ===conteudoDaLinha.includes(expressao)){
+        let conteudoDaLinha = linhas[posicao].innerHTML;
+        if (true === conteudoDaLinha.includes(expressao)){
             linhas[posicao].style.display = '';
         } else {
             linhas[posicao].style.display = 'none';
